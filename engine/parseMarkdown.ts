0a9da0fef13a8920c6992c50e814a180467caed8
davidkadaria <david.qada@gmail.com>
2026-02-06 20:45:07 +0400

    Change parsing functionality and modules

--- engine/parseMarkdown.ts
+++ engine/parseMarkdown.ts
@@ -1,23 +1,16 @@
 import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkGfm from "remark-gfm";
-import remarkFootnotes from "remark-footnotes";
-import remarkRehype from "remark-rehype";
-import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeStringify from "rehype-stringify";
-import type { Plugin } from "unified";
+import remarkRehype from "remark-rehype";
 
 export async function parseMarkdown(markdown: string): Promise<string> {
   const file = await unified()
     .use(remarkParse)
     .use(remarkGfm)
-    .use(remarkFootnotes as Plugin<[{ inlineNotes: boolean }]>, {
-      inlineNotes: true,
+    .use(remarkRehype, {
+      footnoteLabel: "შენიშვნები",
     })
-    .use(remarkRehype)
-    .use(rehypeSlug)
-    .use(rehypeAutolinkHeadings, { behavior: "wrap" })
     .use(rehypeStringify)
     .process(markdown);
 
